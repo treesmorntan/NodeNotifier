425b3ff7400cca68b9aa58aad71889e32dec6fa2
treesmorntan <147564039+treesmorntan@users.noreply.github.com>
2025-09-01 10:56:19 +0700

    Delete emailscript.sh

--- emailscript.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-# this will be a file to test the email automation when the node went down
-to="tanboont9801@uwec.edu"
-from="tanboont9801@uwec.edu"
-
-
-./heredoc-cleanup.sh | mailx -s "List of down node" tanboont9801@uwec.edu
